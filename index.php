041aa1dbd525ad73bfe9e76dfe418cd97bf19273
Kelviny H <kelvinyhenrique17@gmail.com>
2020-05-06 10:08:55 -0300

    Estrutura básica

---
+++ index.php
@@ -0,0 +1,30 @@
+    
+    <?php require './assets/php/classes/header.php'; ?>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h2>Nos temos hoje 99 anúncios</h2>
+            <p>E mais de 9999 usúarios cadastrados</p>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Pesquisa Avançada</h4>
+            </div>
+            <div class="col-sm-9">
+                <h4>Últimos anúncios</h4>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="./assets/js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <?php require './assets/php/classes/footer.php'; ?>
